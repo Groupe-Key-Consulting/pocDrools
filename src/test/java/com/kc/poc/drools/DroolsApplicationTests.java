fc89fccc41b842d6dc51389f8a141614d8783c92
abertin <antoine.bertin@keyconsulting.fr>
2024-03-01 18:32:09 +0100

    chore(init): init poc drools with archetype module exec-model-roleunit and have a simple test to understand structure and syntax of .drl file

---
+++ src/test/java/com/kc/poc/drools/DroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package java.com.kc.poc.drools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
